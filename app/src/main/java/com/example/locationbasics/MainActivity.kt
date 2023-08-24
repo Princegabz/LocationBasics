2193001f8fd19a5b18ccc02077979fbf6d0ccbcd
Gabriel <83117210+Princegabz@users.noreply.github.com>
2023-08-24 09:17:16 +0200

    updated main activity

--- app/src/main/java/com/example/locationbasics/MainActivity.kt
+++ app/src/main/java/com/example/locationbasics/MainActivity.kt
@@ -11,6 +11,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+ import android.widget.Toast
  import androidx.core.app.ActivityCompat
  import androidx.core.content.ContextCompat
 
@@ -31,16 +32,31 @@ import android.widget.TextView
         locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
        if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) !=PackageManager.PERMISSION_GRANTED)
        {
-           ActivityCompat.requestPermissions(this, arrayof(Manifest.permission.ACCESS_FINE_LOCATION),locationPermissionCode)
+           ActivityCompat.requestPermissions(this,arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),locationPermissionCode)
        }
         locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000,5f,this)
     }
 
-     private fun arrayof(accessFineLocation: String): Array<String> {
 
+     override fun onLocationChanged(location: Location) {
+        tvOutput = findViewById(R.id.editTextText)
+         tvOutput.text = "Latitude: "+location.latitude+", Longitude: "+location.longitude
+    }
+
+     override fun onRequestPermissionsResult(
+         requestCode: Int,
+         permissions: Array<out String>,
+         grantResults: IntArray
+     ) {
+         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+         if(grantResults.isNotEmpty()&&grantResults[0]==PackageManager.PERMISSION_GRANTED)
+         {
+             Toast.makeText(this,"Permission Granted", Toast.LENGTH_SHORT).show()
+         }
+         else
+         {
+             Toast.makeText(this,"Permission Denied", Toast.LENGTH_SHORT).show()
+         }
      }
 
-     override fun onLocationChanged(p0: Location) {
-        TODO("Not yet implemented")
-    }
 }
