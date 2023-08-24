f25fb5fe67e250b6baa4760d9022a577ef7f2cd7
Gabriel <83117210+Princegabz@users.noreply.github.com>
2023-08-24 09:09:27 +0200

    updated main activity

--- app/src/main/java/com/example/locationbasics/MainActivity.kt
+++ app/src/main/java/com/example/locationbasics/MainActivity.kt
@@ -1,11 +1,19 @@
  package com.example.locationbasics
 
-import android.location.Location
+
+ import android.Manifest
+import android.content.Context
+ import android.content.pm.PackageManager
+ import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.TextView
+ import androidx.core.app.ActivityCompat
+ import androidx.core.content.ContextCompat
+
 
  class MainActivity : AppCompatActivity(),LocationListener {
     private lateinit var locationManager: LocationManager
@@ -14,9 +22,25 @@ import android.widget.TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button: Button = findViewById(R.id.btnLocations)
+        button.setOnClickListener{
+            getLocation()
+        }
+    }
+    private fun getLocation(){
+        locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
+       if(ContextCompat.checkSelfPermission(this,Manifest.permission.ACCESS_FINE_LOCATION) !=PackageManager.PERMISSION_GRANTED)
+       {
+           ActivityCompat.requestPermissions(this, arrayof(Manifest.permission.ACCESS_FINE_LOCATION),locationPermissionCode)
+       }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 5000,5f,this)
     }
 
-    override fun onLocationChanged(p0: Location) {
+     private fun arrayof(accessFineLocation: String): Array<String> {
+
+     }
+
+     override fun onLocationChanged(p0: Location) {
         TODO("Not yet implemented")
     }
 }
