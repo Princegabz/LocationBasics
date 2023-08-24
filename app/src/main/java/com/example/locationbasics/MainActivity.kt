6394c42ad61c3eeda750e6928ddabcadfbd2fe1e
Gabriel <83117210+Princegabz@users.noreply.github.com>
2023-08-24 08:57:13 +0200

    updated main activity

--- app/src/main/java/com/example/locationbasics/MainActivity.kt
+++ app/src/main/java/com/example/locationbasics/MainActivity.kt
@@ -1,11 +1,22 @@
  package com.example.locationbasics
 
+import android.location.Location
+import android.location.LocationListener
+import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
-class MainActivity : AppCompatActivity() {
+ class MainActivity : AppCompatActivity(),LocationListener {
+    private lateinit var locationManager: LocationManager
+    private lateinit var tvOutput : TextView
+    private val locationPermissionCode=2
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onLocationChanged(p0: Location) {
+        TODO("Not yet implemented")
     }
 }
